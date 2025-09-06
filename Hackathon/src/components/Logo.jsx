5a9abfb199931cebc7ac6358070c517d6b179673
Shivang <kshivang679@gmail.com>
2025-09-07 04:35:18 +0530

    Add logo component with styled image and layout

--- Hackathon/src/components/Logo.jsx
+++ Hackathon/src/components/Logo.jsx
@@ -1,8 +1,17 @@
 import React from "react";
-import viteLogo from "../../public/vite.svg";
+import logo from "../../public/vite.svg";
 
-export default function Logo(){
-    return(
-        <img src={viteLogo} alt="Vite logo" />
-    )
+export default function Logo() {
+  return (
+    <div className="flex items-center">
+      {/* Logo Image */}
+      <div className="w-8 h-8 md:w-10 md:h-10 rounded-full overflow-hidden border-2 border-pink-500 shadow-lg">
+        <img
+          src={logo}
+          alt="HackDevs Logo"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </div>
+  );
 }
